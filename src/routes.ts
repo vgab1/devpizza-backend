4af893cb7c0c2d518c6f16941b20870481f0715a
vgab1 <vgabriel.sousa123@gmail.com>
2025-03-17 17:21:15 -0300

    configurando o usuario

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,12 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
+
+import { CreateUserController } from "./controllers/user/CreateUserController";
+
+import { AuthUserController } from "./controllers/user/AuthUserController";
 
 const router = Router();
 
-router.get("/teste", (req: Request, res: Response) => {
-  throw new Error("Erro ao fazer essa requisição");
-});
+router.post("/users", new CreateUserController().handle);
+router.post("/session", new AuthUserController().handle);
 
 export { router };
