db4edfcd303a189cf931406378b38896dd3999cd
vgab1 <vgabriel.sousa123@gmail.com>
2025-03-21 10:49:26 -0300

    exibindo categorias

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,18 @@
 import { Router } from "express";
+import multer from "multer";
 
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
+import { CreateProductController } from "./controllers/product/CreateProductController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
+import uploadConfig from "./config/multer";
 
 const router = Router();
+const upload = multer(uploadConfig.upload("./tmp"))
+
 // ROTA DE USER
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
@@ -20,5 +25,8 @@ router.post(
   new CreateCategoryController().handle
 );
 router.get("/category", isAuthenticated, new ListCategoryController().handle);
+
+// ROTA DE PRODUCT
+router.post("/product", isAuthenticated, upload.single("file"), new CreateProductController().handle);
 
 export { router };
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response, NextFunction } from "express";
 import "express-async-errors";
 import Cors from "cors";
+import path from "path";
 import { router } from "./routes";
 
 const app = express();
@@ -8,6 +9,8 @@ app.use(express.json());
 app.use(Cors());
 
 app.use(router);
+
+app.use("/files", express.static(path.resolve(__dirname, "..", "tmp")));
 
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   if (err instanceof Error) {
